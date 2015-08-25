708e7ba83d6958dc00233e1b2a51211ecd8e7976
Zoltan Ujhelyi <ujhelyiz@incquerylabs.com>
2015-08-25 18:20:24 +0200

    Added new methods to the parser api for opaque expression parsing #7

--- plugins/com.incquerylabs.uml.ralf/src/com/incquerylabs/uml/ralf/api/IReducedAlfParser.java
+++ plugins/com.incquerylabs.uml.ralf/src/com/incquerylabs/uml/ralf/api/IReducedAlfParser.java
@@ -1,11 +1,16 @@
 package com.incquerylabs.uml.ralf.api;
 
+import org.eclipse.incquery.runtime.api.IncQueryEngine;
 import org.eclipse.uml2.uml.OpaqueBehavior;
+import org.eclipse.uml2.uml.OpaqueExpression;
 
 import com.incquerylabs.uml.ralf.api.impl.ParsingResults;
 import com.incquerylabs.uml.ralf.scoping.IUMLContextProvider;
 
 public interface IReducedAlfParser {
+
+	static final String LANGUAGE_NAME = "rALF";
+
 	/**
 	 * Creates a rALF AST based on the specified rALF code. This AST cannot
 	 * refer to UML types except the primitive types.
@@ -28,11 +33,37 @@ public interface IReducedAlfParser {
 	/**
 	 * Extracts the rALF code from a specified OpaqueBehavior instance, and
 	 * creates the corresponding rALF AST. The UML context provider is
-	 * initialized based on the behavior parameter.
+	 * initialized based on the behavior parameter. It will use an
+	 * IncQuery-based context provider using the managed engine on the
+	 * resourceset of the behavior
 	 * 
 	 * @param behavior
 	 * @return
 	 */
 	ParsingResults parse(OpaqueBehavior behavior);
+
+	/**
+	 * Extracts the rALF code from a specified OpaqueBehavior instance, and
+	 * creates the corresponding rALF AST. The UML context provider is
+	 * initialized based on the behavior parameter.
+	 * 
+	 * @param behavior
+	 * @param engine
+	 *            a shared IncQueryEngine instance
+	 * @return
+	 */
+	ParsingResults parse(OpaqueBehavior behavior, IncQueryEngine engine);
+
+	/**
+	 * Extracts the rALF code from a specified OpaqueExpression instance, and
+	 * creates the corresponding rALF AST. The UML context provider is
+	 * initialized based on the behavior parameter.
+	 * 
+	 * @param expression
+	 * @param engine
+	 *            a shared IncQueryEngine instance
+	 * @return
+	 */
+	ParsingResults parse(OpaqueExpression expression, IncQueryEngine engine);
 
 }
--- plugins/com.incquerylabs.uml.ralf/src/com/incquerylabs/uml/ralf/api/impl/ReducedAlfParser.java
+++ plugins/com.incquerylabs.uml.ralf/src/com/incquerylabs/uml/ralf/api/impl/ReducedAlfParser.java
@@ -5,7 +5,9 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.incquery.runtime.api.IncQueryEngine;
 import org.eclipse.incquery.runtime.emf.EMFScope;
 import org.eclipse.incquery.runtime.exception.IncQueryException;
+import org.eclipse.uml2.uml.BodyOwner;
 import org.eclipse.uml2.uml.OpaqueBehavior;
+import org.eclipse.uml2.uml.OpaqueExpression;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
@@ -18,25 +20,27 @@ import com.incquerylabs.uml.ralf.scoping.UMLContextProvider;
 
 public class ReducedAlfParser implements IReducedAlfParser {
 
-    private static final String LANGUAGE_NAME = "rALF";
-	
-	private Injector createInjector(OpaqueBehavior behavior) {
+	private Injector createInjector(final EObject context, final IncQueryEngine engine) {
 		return createInjector(new UMLContextProvider() {
 			
 			@Override
 			protected EObject getContextObject() {
-				return behavior;
+				return context;
 			}
 			
 			@Override
 			protected IncQueryEngine doGetEngine() {
-				try {
-					return IncQueryEngine.on(new EMFScope(behavior.eResource().getResourceSet()));
-				} catch (IncQueryException e) {
-					throw new RuntimeException(e);
-				}
+				return engine;
 			}
 		});
+	}
+	
+	private IncQueryEngine getDefaultManagedEngine(EObject context) {
+		try {
+			return IncQueryEngine.on(new EMFScope(context.eResource().getResourceSet()));
+		} catch (IncQueryException e) {
+			throw new RuntimeException(e);
+		}
 	}
 	
 	private Injector createInjector(final IUMLContextProvider umlContext) {
@@ -50,6 +54,24 @@ public class ReducedAlfParser implements IReducedAlfParser {
 		};
         return Guice.createInjector(module, customizations);
     }
+	
+	private ParsingResults parseBodyOwner(BodyOwner behavior, Injector injector) {
+		int indexOfRALFBody = -1;
+		ParsingResults result = null;
+		for (int i = 0; i < behavior.getLanguages().size() && indexOfRALFBody == -1; i++) {
+			if (behavior.getLanguages().get(i).equals(LANGUAGE_NAME)) {
+				indexOfRALFBody = i;
+			}
+		}
+		EList<String> bodies = behavior.getBodies();
+		if (indexOfRALFBody >= 0) {
+			InternalReducedAlfParser parser = injector.getInstance(InternalReducedAlfParser.class);
+			result = parser.parse(bodies.get(indexOfRALFBody));
+		}
+		
+		return result;
+	}
+	
 	
 	@Override
 	public ParsingResults parse(String behavior) {
@@ -65,20 +87,18 @@ public class ReducedAlfParser implements IReducedAlfParser {
 
 	@Override
 	public ParsingResults parse(OpaqueBehavior behavior) {
-		Injector injector = createInjector(behavior);
-		int indexOfRALFBody = -1;
-        ParsingResults result = null;
-        for (int i = 0; i < behavior.getLanguages().size() && indexOfRALFBody == -1; i++) {
-            if (behavior.getLanguages().get(i).equals(LANGUAGE_NAME)) {
-                indexOfRALFBody = i;
-            }
-        }
-        EList<String> bodies = behavior.getBodies();
-        if (indexOfRALFBody >= 0) {
-        	InternalReducedAlfParser parser = injector.getInstance(InternalReducedAlfParser.class);
-            result = parser.parse(bodies.get(indexOfRALFBody));
-        }
-        
-        return result;
+		return parse(behavior, getDefaultManagedEngine(behavior));
+	}
+
+	@Override
+	public ParsingResults parse(OpaqueBehavior behavior, IncQueryEngine engine) {
+		Injector injector = createInjector(behavior, engine);
+		return parseBodyOwner(behavior, injector);
+	}
+
+	@Override
+	public ParsingResults parse(OpaqueExpression expression, IncQueryEngine engine) {
+		Injector injector = createInjector(expression, engine);
+		return parseBodyOwner(expression, injector);
 	}
 }
