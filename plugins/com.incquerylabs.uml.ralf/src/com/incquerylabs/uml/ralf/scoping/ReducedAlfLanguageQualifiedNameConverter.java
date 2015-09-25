7a5dec03e2b0c8e096bc12a9d0a05e3b195154ea
Zoltan Ujhelyi <ujhelyiz@incquerylabs.com>
2015-09-25 12:55:22 +0200

    Updated name converter to remove apostrophes #68

--- plugins/com.incquerylabs.uml.ralf/src/com/incquerylabs/uml/ralf/scoping/ReducedAlfLanguageQualifiedNameConverter.java
+++ plugins/com.incquerylabs.uml.ralf/src/com/incquerylabs/uml/ralf/scoping/ReducedAlfLanguageQualifiedNameConverter.java
@@ -1,7 +1,12 @@
 package com.incquerylabs.uml.ralf.scoping;
 
+import java.util.Spliterators;
+import java.util.stream.Collectors;
+
 import org.eclipse.xtext.naming.IQualifiedNameConverter.DefaultImpl;
 import org.eclipse.xtext.naming.QualifiedName;
+
+import com.google.common.base.Splitter;
 
 public class ReducedAlfLanguageQualifiedNameConverter extends DefaultImpl {
 
@@ -10,15 +15,14 @@ public class ReducedAlfLanguageQualifiedNameConverter extends DefaultImpl {
 		return "::";
 	}
 
-//	@Override
-//	public QualifiedName toQualifiedName(String qualifiedNameAsString) {
-//		String source;
-//		if (qualifiedNameAsString.startsWith("PrimitiveTypes.")) {
-//			source = qualifiedNameAsString.substring(15);
-//		} else {
-//			source = qual
-//		}
-//		return super.toQualifiedName(qualifiedNameAsString);
-//	}
+	@Override
+	public QualifiedName toQualifiedName(String qualifiedNameAsString) {
+		String fqn = Splitter.on(getDelimiter()).splitToList(qualifiedNameAsString).stream()
+				.map((String segment) -> (segment != null && segment.startsWith("'") && segment.endsWith("'"))
+						? segment.substring(1, segment.length() - 1) : segment)
+				.collect(Collectors.joining("::"));
+
+		return super.toQualifiedName(fqn);
+	}
 
 }
